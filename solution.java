2c3a0060c821665db8a866a5cd2ecef06d146b56
Sanjaiweb <sanjaisanjai7182@gmail.com>
2025-02-12 22:00:56 +0530

    Auto-sync commit

--- solution.java
+++ solution.java
@@ -5,13 +5,20 @@ class A    //parent class
   {
     System.out.println("in A");
   }
-  
+  public A(int n){
+    System.out.println(" In A int");
+  }
 }
 class B extends A    //child class
 {
   public B()
   {
     System.out.println("in B");
+  }
+  public B(int n)
+  {
+    super(n);
+    System.out.println("in B int");
   }
   
 }
@@ -20,7 +27,7 @@ public class solution
 {
     public static void main(String args[]) 
     {
-          B obj = new B();
+          B obj = new B(10);
 
     }
 }
