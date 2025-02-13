84b3301adee940a5d6e228988dbcdcbcef718365
Sanjaiweb <sanjaisanjai7182@gmail.com>
2025-02-13 21:47:11 +0530

    Class over (this , super ,object class ) learnt

--- solution.java
+++ solution.java
@@ -2,10 +2,12 @@
 class A    //parent class
 {
   public A()
-  {
+  { 
+    super();
     System.out.println("in A");
   }
   public A(int n){
+    super();
     System.out.println(" In A int");
   }
 }
@@ -13,11 +15,12 @@ class B extends A    //child class
 {
   public B()
   {
+    super();
     System.out.println("in B");
   }
   public B(int n)
   {
-    super(n);
+    this();
     System.out.println("in B int");
   }
   
