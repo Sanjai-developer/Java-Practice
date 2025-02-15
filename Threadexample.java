e04803e300b47f3206603908e64f5c121559dc1d
Sanjaiweb <sanjaisanjai7182@gmail.com>
2025-02-15 20:37:33 +0530

    Thread USing Runnable Interface Concurrent

--- Threadexample.java
+++ Threadexample.java
@@ -1,9 +1,26 @@
 import java.util.*;
-class Myclass implements Runnable{
+
+// Concurrent Execution
+class task1 implements Runnable{
     public void run(){
         System.out.println(" Threads are Running");
         for(int i=0;i<=5;i++){
-            System.out.println(" Thread : " + i + " is Running");
+            System.out.println(" Thread 1 : " + i + " is Running");
+            try{
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException e){
+                System.out.println("Thread Interrupted");
+            }  
+        }
+        System.out.println("Thread Ended");
+    }
+}
+class task2 implements Runnable{
+    public void run(){
+        System.out.println(" Threads are Running");
+        for(int i=0;i<=6;i++){
+            System.out.println(" Thread 2 : " + i + " is Running");
             try{
                 Thread.sleep(1000);
             }
@@ -16,7 +33,9 @@ class Myclass implements Runnable{
 }
 public class Threadexample {
     public static void main(String[] args){
-        Thread t1 = new Thread(new Myclass());
+        Thread t1 = new Thread(new task1());
+        Thread t2 = new Thread(new task2());
         t1.start();
+        t2.start();
     }
 }
